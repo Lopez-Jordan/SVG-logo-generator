e9a6c4b3d8fa0fe72527a0a77ebf93cae1d32f2b
Jordan Lopez <jordanlopezemail@gmail.com>
2023-07-25 21:53:13 -0700

    set up files

---
+++ index.js
@@ -0,0 +1,14 @@
+// use inquirer to accept user input about:
+    //1: 3 letters (for text of the SVG)
+    //2: text color (color or hex)
+    //3: shape - circle, triangle, square
+    //4: shape color (color or hex)
+
+
+    const createLogo = genLogo();
+// use fs to write file with (logo.svg, createLogo)
+
+
+// NOTES:
+    // svg must be 300x200 pixels
+    // create Triangle, Circle, and Square classes (tested with jest) (as children of Shape)
