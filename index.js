466386f1c55284e7c589057056563d679dbe0122
Jordan Lopez <jordanlopezemail@gmail.com>
2023-07-26 18:21:51 -0700

    fixed the module.exports, now working on tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { Circle, Square, Triangle } = require('./lib/shapes');
+const {Circle, Triangle, Square } = require('./lib/shapes');
 
 
 function promptUser(){
